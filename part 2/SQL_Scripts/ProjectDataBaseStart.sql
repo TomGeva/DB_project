71fbc2eb2aafd1e690afecf09e32a2642c346245
TomGeva <tomgeva7@gmail.com>
2024-02-18 13:35:57 +0200

    final version

--- part 2/SQL_Scripts/ProjectDataBaseStart.sql
+++ part 2/SQL_Scripts/ProjectDataBaseStart.sql
@@ -1,62 +1,67 @@
 -- || TABLE CREATION SECTION ||
 
 CREATE TABLE dbo.PRODUCTS (		-- creating dbo.PRODUCTS table
-	Name			Varchar(40)		PRIMARY KEY		NOT NULL,
-	Price			Smallmoney						NOT NULL,
-	Discount		Smallmoney						NOT NULL	DEFAULT 0
+	Name			Varchar(80)			NOT NULL,
+	Price			Smallmoney			NOT NULL,
+	Discount		Smallmoney			NOT NULL		DEFAULT 0,
+
+	CONSTRAINT	Pk_product		PRIMARY KEY		(Name)
 )
 
 CREATE TABLE dbo.RELATIONS (	-- creating dbo.RELATIONS table
-	Product1		Varchar(40)						NOT NULL,
-	Product2		Varchar(40)						NOT NULL,
+	Product1		Varchar(80)			NOT NULL,
+	Product2		Varchar(80)			NOT NULL,
 
-	CONSTRAINT	Pk_relation		PRIMARY KEY	(Product1, Product2),
-	CONSTRAINT	Fk_Product1		FOREIGN KEY	(Product1)
+	CONSTRAINT	Pk_relation		PRIMARY KEY		(Product1, Product2),
+	CONSTRAINT	Fk_Product1		FOREIGN KEY		(Product1)
 									REFERENCES	dbo.PRODUCTS	(Name),
-	CONSTRAINT	Fk_Product2		FOREIGN KEY	(Product2)
+	CONSTRAINT	Fk_Product2		FOREIGN KEY		(Product2)
 									REFERENCES	dbo.PRODUCTS	(Name)
 )
 
 CREATE TABLE dbo.USERS (		-- creating dbo.USERS table
-	Email			Varchar(40)		PRIMARY KEY		NOT NULL,
-	Password		Varchar(30)						NOT NULL
+	Email			Varchar(40)			NOT NULL,
+	Password		Varchar(30)			NOT NULL,
+
+	CONSTRAINT	Pk_user			PRIMARY KEY		(Email)
 )
 
 CREATE TABLE dbo.SEARCHES (		-- creating dbo.SEARCHES table
-	SearchDT		Datetime						NOT NULL,
-	IP_address		Varchar(15)						NOT NULL,
-	Email			Varchar(40)						NULL,
-	Search_text		Varchar(80)						NOT NULL,
+	SearchDT		Datetime			NOT NULL,
+	IP_address		Varchar(15)			NOT NULL,
+	Email			Varchar(40)			NULL,
+	Search_text		Varchar(80)			NOT NULL,
 
-	CONSTRAINT	Pk_searches		PRIMARY KEY	(SearchDT, IP_address),
-	CONSTRAINT	Fk_user_srch	FOREIGN KEY	(Email)
+	CONSTRAINT	Pk_search		PRIMARY KEY		(SearchDT, IP_address),
+	CONSTRAINT	Fk_user_srch	FOREIGN KEY		(Email)
 									REFERENCES	dbo.USERS		(Email)
 )
 
 CREATE TABLE dbo.RESULTS (		-- creating dbo.RESULTS table
-	Name			Varchar(40)						NOT NULL,
-	SearchDT		Datetime						NOT NULL,
-	IP_address		Varchar(15)						NOT NULL,
+	Name			Varchar(80)			NOT NULL,
+	SearchDT		Datetime			NOT NULL,
+	IP_address		Varchar(15)			NOT NULL,
 
-	CONSTRAINT	Pk_results		PRIMARY KEY (Name, SearchDT, IP_address),
-	CONSTRAINT	Fk_product		FOREIGN KEY	(Name)
+	CONSTRAINT	Pk_result		PRIMARY KEY		(Name, SearchDT, IP_address),
+	CONSTRAINT	Fk_product		FOREIGN KEY		(Name)
 									REFERENCES	dbo.PRODUCTS	(Name),
-	CONSTRAINT	Fk_search		FOREIGN KEY	(SearchDT, IP_address)
+	CONSTRAINT	Fk_search		FOREIGN KEY		(SearchDT, IP_address)
 									REFERENCES	dbo.SEARCHES	(SearchDT, IP_address)
 )
 
 CREATE TABLE dbo.SEEDS (		-- creating dbo.SEEDS table
-	Name			Varchar(40)		PRIMARY KEY		NOT NULL,
-	Size			Char(5)							NOT NULL,
-	Season			Varchar(6)						NOT NULL,
-	Sun_amount		Varchar(30)						NOT NULL,
+	Name			Varchar(80)			NOT NULL,
+	Size			Char(5)				NOT NULL,
+	Season			Varchar(6)			NOT NULL,
+	Sun_amount		Varchar(30)			NOT NULL,
 
-	CONSTRAINT	Fk_Name_seed	FOREIGN KEY	(Name)
+	CONSTRAINT	Pk_seed			PRIMARY KEY		(Name),
+	CONSTRAINT	Fk_Name_seed	FOREIGN KEY		(Name)
 									REFERENCES	dbo.PRODUCTS	(Name)
 )
 
 CREATE TABLE dbo.SEED_TYPES (	-- creating dbo.SEED_TYPES table
-	Name			Varchar(40)						NOT NULL,
+	Name			Varchar(80)						NOT NULL,
 	Type			Varchar(20)						NOT NULL,
 
 	CONSTRAINT	Pk_seed_type	PRIMARY KEY	(Name, Type),
@@ -65,71 +70,73 @@ CREATE TABLE dbo.SEED_TYPES (	-- creating dbo.SEED_TYPES table
 )
 
 CREATE TABLE dbo.GARDENS (		-- creating dbo.GARDENS table
-	Name			Varchar(40)		PRIMARY KEY		NOT NULL,
-	Small_count		Tinyint							NOT NULL,
-	Large_count		Tinyint							NOT NULL,
+	Name			Varchar(80)			NOT NULL,
+	Small_count		Tinyint				NOT NULL,
+	Large_count		Tinyint				NOT NULL,
 
-	CONSTRAINT	Fk_Name_Garden	FOREIGN KEY	(Name)
+	CONSTRAINT	Pk_garden		PRIMARY KEY		(Name),
+	CONSTRAINT	Fk_Name_Grdn	FOREIGN KEY		(Name)
 									REFERENCES	dbo.PRODUCTS	(Name)
 )
 
 CREATE TABLE dbo.CHOSENS (		-- creating dbo.CHOSENS table
-	Garden			Varchar(40)						NOT NULL,
-	Seed			Varchar(40)						NOT NULL,
-	Quantity		Tinyint							NOT NULL	DEFAULT 1,
+	Garden			Varchar(80)			NOT NULL,
+	Seed			Varchar(80)			NOT NULL,
+	Quantity		Tinyint				NOT NULL		DEFAULT 1,
 
-	CONSTRAINT	Pk_chosen		PRIMARY KEY	(Garden, Seed),
-	CONSTRAINT	Fk_Garden		FOREIGN KEY	(Garden)
+	CONSTRAINT	Pk_chosen		PRIMARY KEY		(Garden, Seed),
+	CONSTRAINT	Fk_Garden		FOREIGN KEY		(Garden)
 									REFERENCES	dbo.GARDENS		(Name),
-	CONSTRAINT	Fk_Seed_chs		FOREIGN KEY	(Seed)
+	CONSTRAINT	Fk_Seed_chs		FOREIGN KEY		(Seed)
 									REFERENCES	dbo.SEEDS		(Name)
 )
 
 CREATE TABLE dbo.DETAILS (		-- creating dbo.DETAILS table
-	Name			Varchar(40)						NOT NULL,
-	Address			Varchar(150)					NOT NULL,
-	Company			Varchar(40)						NULL,
-	Phone#			Varchar(30)						NOT NULL,
+	Name			Varchar(40)			NOT NULL,
+	Address			Varchar(150)		NOT NULL,
+	Company			Varchar(40)			NULL,
+	Phone#			Varchar(30)			NOT NULL,
 
-	CONSTRAINT	Pk_details		PRIMARY KEY	(Name, Address)
+	CONSTRAINT	Pk_details		PRIMARY KEY		(Name, Address)
 )
 
 CREATE TABLE dbo.DETAILS_OF (	-- creating dbo.DETAILS_OF table
-	Email			Varchar(40)						NOT NULL,
-	Name			Varchar(40)						NOT NULL,
-	Address			Varchar(150)					NOT NULL,
+	Email			Varchar(40)			NOT NULL,
+	Name			Varchar(40)			NOT NULL,
+	Address			Varchar(150)		NOT NULL,
 
-	CONSTRAINT	Pk_details_of	PRIMARY KEY	(Email, Name, Address),
-	CONSTRAINT	Fk_user_detls	FOREIGN KEY	(Email)
+	CONSTRAINT	Pk_dtlsOf		PRIMARY KEY		(Email, Name, Address),
+	CONSTRAINT	Fk_dtls_usr		FOREIGN KEY		(Email)
 									REFERENCES	dbo.USERS		(Email),
-	CONSTRAINT	Fk_details_usr	FOREIGN KEY	(Name, Address)
+	CONSTRAINT	Fk_detOf_usr	FOREIGN KEY		(Name, Address)
 									REFERENCES	dbo.DETAILS		(Name, Address)
 )
 
 CREATE TABLE dbo.ORDERS (		-- creating dbo.ORDERS table
-	OrderID			Int				PRIMARY KEY		NOT NULL,
-	Email			Varchar(40)						NULL,
-	Name			Varchar(40)						NOT NULL,
-	Address			Varchar(150)					NOT NULL,
-	OrderDate		Date							NOT NULL,
-	Shipping_method	Varchar(100)					NULL,
-	Payment_type	Varchar(7)						NOT NULL,
+	OrderID			Int					NOT NULL,
+	Email			Varchar(40)			NULL,
+	Name			Varchar(40)			NOT NULL,
+	Address			Varchar(150)		NOT NULL,
+	OrderDate		Date				NOT NULL,
+	Shipping_method	Varchar(100)		NULL,
+	Payment_type	Varchar(7)			NOT NULL,
 
-	CONSTRAINT	Fk_user_ordr	FOREIGN KEY	(Email)
+	CONSTRAINT	Pk_order		PRIMARY KEY		(OrderID),
+	CONSTRAINT	Fk_user_ordr	FOREIGN KEY		(Email)
 									REFERENCES	dbo.USERS		(Email),
-	CONSTRAINT	Fk_details_ordr	FOREIGN KEY	(Name, Address)
+	CONSTRAINT	Fk_dtls_ordr	FOREIGN KEY		(Name, Address)
 									REFERENCES	dbo.DETAILS		(Name, Address)
 )
 
 CREATE TABLE dbo.INCLUSIONS (	-- creating dbo.INCLUSIONS table
-	OrderID			Int								NOT NULL,
-	Name			Varchar(40)						NOT NULL,
-	Quantity		Tinyint							NOT NULL	DEFAULT 1,
+	OrderID			Int					NOT NULL,
+	Name			Varchar(80)			NOT NULL,
+	Quantity		Tinyint				NOT NULL		DEFAULT 1,
 
-	CONSTRAINT	Pk_inclusion	PRIMARY KEY	(OrderID, Name),
-	CONSTRAINT	Fk_order_ncl	FOREIGN KEY	(OrderID)
+	CONSTRAINT	Pk_inclusion	PRIMARY KEY		(OrderID, Name),
+	CONSTRAINT	Fk_ordr_ncl		FOREIGN KEY		(OrderID)
 									REFERENCES	dbo.ORDERS		(OrderID),
-	CONSTRAINT	Fk_product_ncl	FOREIGN KEY	(Name)
+	CONSTRAINT	Fk_prdc_ncl		FOREIGN KEY		(Name)
 									REFERENCES	dbo.PRODUCTS	(Name)
 )
 
@@ -138,83 +145,104 @@ CREATE TABLE dbo.INCLUSIONS (	-- creating dbo.INCLUSIONS table
 -- contrsaints for inforcing values of price and discount of products
 
 ALTER TABLE dbo.PRODUCTS
-	ADD CONSTRAINT	Ck_Price		CHECK		(Price > 0),
-		CONSTRAINT	Ck_Discount		CHECK		(Discount < Price)
+	ADD	CONSTRAINT	Ck_Price
+			CHECK	(Price > 0),
+		CONSTRAINT	Ck_Discount
+			CHECK	(Discount < Price)
 
 -- constraint that inforces Email format
 
 ALTER TABLE dbo.USERS
-	ADD	CONSTRAINT	Ck_Email		CHECK		(Email LIKE '%@%.%')
+	ADD	CONSTRAINT	Ck_Email
+			CHECK	(Email LIKE '%@%.%')
 
 -- constraint that inforces ip format
 
 ALTER TABLE dbo.SEARCHES
-	ADD CONSTRAINT	Ck_IP_address	CHECK		((ParseName(IP_address, 4) BETWEEN 0 AND 255) 
-												AND(ParseName(IP_address, 3) BETWEEN 0 AND 255)
-												AND(ParseName(IP_address, 2) BETWEEN 0 AND 255)
-												AND(ParseName(IP_address, 1) BETWEEN 0 AND 255))
+	ADD	CONSTRAINT	Ck_IP_address
+			CHECK	((ParseName(IP_address, 4) BETWEEN 0 AND 255) 
+					AND(ParseName(IP_address, 3) BETWEEN 0 AND 255)
+					AND(ParseName(IP_address, 2) BETWEEN 0 AND 255)
+					AND(ParseName(IP_address, 1) BETWEEN 0 AND 255))
 
 -- constraints that inforces values of Size and Season
 
 ALTER TABLE dbo.SEEDS
-	ADD	CONSTRAINT	Ck_Size			CHECK		(Size IN ('Small', 'Large')),
-		CONSTRAINT	Ck_Season		CHECK		(Season IN ('Summer', 'Spring', 'Winter', 'Fall'))
+	ADD	CONSTRAINT	Ck_Size
+			CHECK	(Size IN ('Small', 'Large')),
+		CONSTRAINT	Ck_Season
+			CHECK	(Season IN ('Summer', 'Spring', 'Winter', 'Fall'))
 
 -- constraints that inforces values of Small_count and Large_count
 
 ALTER TABLE dbo.GARDENS		
-	ADD	CONSTRAINT	Ck_counts		CHECK		((Small_count = 2 AND Large_count = 2)
-												OR (Small_count = 5 AND Large_count = 1)
-												OR (Small_count = 8 AND Large_count = 0))
+	ADD	CONSTRAINT	Ck_counts
+			CHECK	((Small_count = 2 AND Large_count = 2)
+					OR (Small_count = 5 AND Large_count = 1)
+					OR (Small_count = 8 AND Large_count = 0))
 
--- constraint that inforces vakue of quantity
+-- constraint that inforces value of quantity
 
 ALTER TABLE dbo.CHOSENS
-	ADD	CONSTRAINT	Ck_Quantity_chs	CHECK		(Quantity > 0)
+	ADD	CONSTRAINT	Ck_Quantity_chs
+			CHECK	(Quantity > 0)
 
 -- add constraint to inforce phone# format
 
 ALTER TABLE dbo.DETAILS
-	ADD CONSTRAINT	Ck_Phone#		CHECK		(Phone# LIKE '[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]')
-
--- add constraint to inforce values of payment_type 
-
-ALTER TABLE dbo.ORDERS
-	ADD CONSTRAINT	Ck_Payment_type	CHECK		(Payment_type IN ('Klarna', 'ShopPay'))
+	ADD	CONSTRAINT	Ck_Phone#
+			CHECK	(Phone# LIKE '[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]')
 
 -- add constraint to inforce values of quantity
 
 ALTER TABLE dbo.INCLUSIONS
-	ADD CONSTRAINT	Ck_Quantity_ncl	CHECK		(Quantity > 0)
+	ADD	CONSTRAINT	Ck_Quantity_ncl
+			CHECK	(Quantity > 0)
 
 -- || LOOKUP TABLES CREATION ||
 
+-- add seed type lookup table
+
 CREATE TABLE dbo.SEEDTYPELOOKUP (
-	Type			Varchar(20)		PRIMARY KEY		NOT NULL,
+	Type			Varchar(20)		PRIMARY KEY		NOT NULL
 )
 
-INSERT INTO dbo.SEEDTYPELOOKUP VALUES
+INSERT INTO dbo.SEEDTYPELOOKUP 
+VALUES
 ('Greens'), ('Herbs'), ('Fruiting'), ('Flowers'), ('Root Vegetables'), ('Heirloom')
 
 ALTER TABLE dbo.SEED_TYPES
-	ADD	CONSTRAINT	Fk_seedtype	FOREIGN KEY (Type)	REFERENCES	dbo.SEEDTYPELOOKUP (Type)
+	ADD	CONSTRAINT	Fk_seedtypeLU	FOREIGN KEY (Type)	
+									REFERENCES	dbo.SEEDTYPELOOKUP (Type)
+
+-- add payment type lookup table
+
+CREATE TABLE dbo.PYMNTTYPELOOKUP (
+	Type			Varchar(7)		PRIMARY KEY		NOT NULL
+)
+
+INSERT INTO dbo.PYMNTTYPELOOKUP
+VALUES
+('ShopPay'), ('Klarna')
+
+ALTER TABLE dbo.ORDERS
+	ADD CONSTRAINT	Fk_Pymnt_type	FOREIGN KEY	(Payment_type)	
+										REFERENCES	dbo.PYMNTTYPELOOKUP	(Type)
 
 -- || DATA INSERTION SECTION ||
 
 -- insertion to dbo.PRODUCTS
 
-INSERT INTO dbo.PRODUCTS (Name, Price)
+INSERT INTO dbo.PRODUCTS
 VALUES
-('Fennel', 12),
-('Scallion', 10),
-('Tomato: Heirloom Purple', 10),
-('Sunflower: Golden', 12),
-('Pepper: Large Bell', 9)
-
-INSERT INTO dbo.PRODUCTS (Name, Price, Discount)
-VALUES
+('Fennel', 12, 0),
+('Scallion', 10, 0),
+('Tomato: Heirloom Purple', 10, 0),
+('Sunflower: Golden', 12, 0),
+('Pepper: Large Bell', 9, 0),
 ('Light Salsa Garden', 119.99, 20),
 ('Root & Vegi Salad Garden', 129.99, 25),
+('Simple Vegi Salad Garden', 129.99, 25),
 ('Custom: SnflwrGldn2, PprLB2', 159.99, 30),
 ('Custom: Scln3, Fnl2, SnflrGldn3', 159.99, 10),
 ('SUPER Fertilizer', 34.99, 3.75),
@@ -222,7 +250,7 @@ VALUES
 
 -- insertion to dbo.RELATIONS
 
-INSERT INTO dbo.RELATIONS (Product1, Product2)
+INSERT INTO dbo.RELATIONS
 VALUES
 ('SUPER Fertilizer', 'Light Salsa Garden'),
 ('SUPER Fertilizer', 'Root & Vegi Salad Garden'),
@@ -234,7 +262,7 @@ VALUES
 
 -- insertion to dbo.USERS
 
-INSERT INTO dbo.USERS (Email, Password)
+INSERT INTO dbo.USERS
 VALUES
 ('tomge@post.bgu.ac.il', 'Mis63677'),
 ('sophiada@post.bgu.ac.il', 'Mis99988'),
@@ -244,7 +272,7 @@ VALUES
 
 -- insertion to dbo.SEARCHES
 
-INSERT INTO dbo.SEARCHES (SearchDT, IP_address, Email, Search_text)
+INSERT INTO dbo.SEARCHES
 VALUES
 ('1999-03-16 13:57:22', '10.100.102.13', 'mickeyM@walla.co.il', 'Seed'),
 ('2006-04-29 10:30:12', '255.30.2.0', NULL, 'Fertilizer'),
@@ -255,7 +283,7 @@ VALUES
 
 -- insertion to dbo.RESULTS
 
-INSERT INTO dbo.RESULTS (Name, SearchDT, IP_address)
+INSERT INTO dbo.RESULTS
 VALUES
 ('Fennel', '1999-03-16 13:57:22', '10.100.102.13'),
 ('Scallion', '1999-03-16 13:57:22', '10.100.102.13'),
@@ -267,7 +295,7 @@ VALUES
 
 -- insertion to dbo.SEEDS
 
-INSERT INTO dbo.SEEDS (Name, Size, Season, Sun_amount)
+INSERT INTO dbo.SEEDS
 VALUES
 ('Fennel', 'Small', 'Spring', 'Full Sun / Partial Shade'),
 ('Scallion', 'Small', 'Winter', 'Partial Shade'),
@@ -277,7 +305,7 @@ VALUES
 
 -- insertion to dbo.SEED_TYPES
 
-INSERT INTO dbo.SEED_TYPES (Name, Type)
+INSERT INTO dbo.SEED_TYPES
 VALUES
 ('Fennel', 'Greens'),
 ('Fennel', 'Fruiting'),
@@ -290,16 +318,17 @@ VALUES
 
 -- insertion to dbo.GARDENS
 
-INSERT INTO dbo.GARDENS (Name, Small_count, Large_count)
+INSERT INTO dbo.GARDENS
 VALUES
 ('Light Salsa Garden', 2, 2),
 ('Root & Vegi Salad Garden', 5, 1),
+('Simple Vegi Salad Garden', 2, 2),
 ('Custom: SnflwrGldn2, PprLB2', 2, 2),
 ('Custom: Scln3, Fnl2, SnflrGldn3', 8, 0)
 
 -- insertion to dbo.CHOSENS
 
-INSERT INTO dbo.CHOSENS (Garden, Seed, Quantity)
+INSERT INTO dbo.CHOSENS
 VALUES
 ('Light Salsa Garden', 'Pepper: Large Bell', 1),
 ('Light Salsa Garden', 'Tomato: Heirloom Purple', 1),
@@ -307,6 +336,10 @@ VALUES
 ('Root & Vegi Salad Garden', 'Fennel', 2),
 ('Root & Vegi Salad Garden', 'Tomato: Heirloom Purple', 1),
 ('Root & Vegi Salad Garden', 'Scallion', 3),
+('Simple Vegi Salad Garden', 'Tomato: Heirloom Purple', 1),
+('Simple Vegi Salad Garden', 'Pepper: Large Bell', 1),
+('Simple Vegi Salad Garden', 'Fennel', 1),
+('Simple Vegi Salad Garden', 'Scallion', 1),
 ('Custom: SnflwrGldn2, PprLB2', 'Sunflower: Golden', 2),
 ('Custom: SnflwrGldn2, PprLB2', 'Pepper: Large Bell', 2),
 ('Custom: Scln3, Fnl2, SnflrGldn3', 'Sunflower: Golden', 3),
@@ -315,7 +348,7 @@ VALUES
 
 -- insertion to dbo.DETAILS
 
-INSERT INTO dbo.DETAILS (Name, Address, Company, Phone#)
+INSERT INTO dbo.DETAILS
 VALUES
 ('Mickey H Mouse the 1st', 'United States, California, Disney Land, , , 00000001', 'Disney', '1234598889'),
 ('Goofy & Pluto', 'United States, California, Disney Land, , , 00000001', 'Disney', '0126549854'),
@@ -326,7 +359,7 @@ VALUES
 
 -- insertion to dbo.DETAILS_OF
 
-INSERT INTO dbo.DETAILS_OF (Email, Name, Address)
+INSERT INTO dbo.DETAILS_OF
 VALUES
 ('mickeyM@walla.co.il', 'Mickey H Mouse the 1st', 'United States, California, Disney Land, , , 00000001'),
 ('mickeyM@walla.co.il', 'Goofy & Pluto', 'United States, California, Disney Land, , , 00000001'),
@@ -336,7 +369,7 @@ VALUES
 
 -- insertion to dbo.ORDERS
 
-INSERT INTO dbo.ORDERS (OrderID, Email, Name, Address, OrderDate, Shipping_method, Payment_type)
+INSERT INTO dbo.ORDERS
 VALUES
 (33222, 'mickeyM@walla.co.il', 'Mickey H Mouse the 1st', 'United States, California, Disney Land, , , 00000001', '1999-03-16', 'Pickup from the factory', 'Klarna'),
 (33223, NULL, 'Laura Welch', 'United States, Texas, Alburkurky, Midland st., 8, 23615656', '2009-03-16', NULL, 'Klarna'),
@@ -348,7 +381,7 @@ VALUES
 
 -- insertion to dbo.INCLUSIONS
 
-INSERT INTO dbo.INCLUSIONS (OrderID, Name, Quantity)
+INSERT INTO dbo.INCLUSIONS
 VALUES
 (33222, 'Custom: SnflwrGldn2, PprLB2', 3),
 (33222, 'Custom: Scln3, Fnl2, SnflrGldn3', 3),
@@ -359,33 +392,103 @@ VALUES
 (33227, 'Pruning & Harvesting Scissors', 5),
 (33228, 'Sunflower: Golden', 30)
 
-
 -- || DROPING OF TABLES SECTION ||
 
-DROP TABLE dbo.INCLUSIONS		-- creating dbo.INCLUSIONS table
+-- droping dbo.INCLUSIONS table and its constraints
 
-DROP TABLE dbo.ORDERS			-- droping dbo.ORDERS table
+ALTER TABLE dbo.INCLUSIONS
+	DROP CONSTRAINT		Pk_inclusion, Fk_ordr_ncl, Fk_prdc_ncl, Ck_Quantity_ncl
 
-DROP TABLE dbo.DETAILS_OF		-- droping dbo.DETAILS_OF table
+DROP TABLE dbo.INCLUSIONS
 
-DROP TABLE dbo.DETAILS			-- droping dbo.DETAILS table
+-- droping dbo.ORDERS table and its constraints
 
-DROP TABLE dbo.CHOSENS			-- droping dbo.CHOSENS table
+ALTER TABLE dbo.ORDERS
+	DROP CONSTRAINT		Pk_order, Fk_user_ordr, Fk_dtls_ordr, Fk_Pymnt_type
 
-DROP TABLE dbo.GARDENS			-- droping dbo.GARDENS table
+DROP TABLE dbo.ORDERS
 
-DROP TABLE dbo.SEED_TYPES		-- droping dbo.SEED_TYPES table
+-- droping dbo.PYMNTTYPELOOKUP table
 
-DROP TABLE dbo.SEEDTYPELOOKUP	-- droping dbo.SEEDTYPELOOKUP table
+DROP TABLE dbo.PYMNTTYPELOOKUP
 
-DROP TABLE dbo.SEEDS			-- droping dbo.SEEDS table
+-- droping dbo.DETAILS_OF table and its constraints
 
-DROP TABLE dbo.RESULTS			-- droping dbo.RESULTS table
+ALTER TABLE dbo.DETAILS_OF
+	DROP CONSTRAINT		Pk_dtlsOf, Fk_dtls_usr, Fk_detOf_usr
 
-DROP TABLE dbo.SEARCHES			-- droping dbo.SEARCHES table
+DROP TABLE dbo.DETAILS_OF
 
-DROP TABLE dbo.USERS			-- droping dbo.USERS table
+-- droping dbo.DETAILS table and its constraints
 
-DROP TABLE dbo.RELATIONS		-- droping dbo.RELATIONS table
+ALTER TABLE dbo.DETAILS
+	DROP CONSTRAINT		Pk_details, Ck_Phone#
 
-DROP TABLE dbo.PRODUCTS			-- droping dbo.PRODUCTS table
+DROP TABLE dbo.DETAILS
+
+-- droping dbo.CHOSENS table and its constraints
+
+ALTER TABLE dbo.CHOSENS
+	DROP CONSTRAINT		Pk_chosen, Fk_Garden, Fk_Seed_chs, Ck_Quantity_chs
+
+DROP TABLE dbo.CHOSENS
+
+-- droping dbo.GARDENS table and its constraints
+
+ALTER TABLE dbo.GARDENS
+	DROP CONSTRAINT		Pk_garden, Fk_Name_Grdn, Ck_counts
+
+DROP TABLE dbo.GARDENS
+
+-- droping dbo.SEED_TYPES table and its constraints
+
+ALTER TABLE dbo.SEED_TYPES
+	DROP CONSTRAINT		Pk_seed_type, Fk_seed, Fk_seedtypeLU
+
+DROP TABLE dbo.SEED_TYPES
+
+-- droping dbo.SEEDTYPELOOKUP table
+
+DROP TABLE dbo.SEEDTYPELOOKUP
+
+-- droping dbo.SEEDS table and its constraints
+
+ALTER TABLE dbo.SEEDS
+	DROP CONSTRAINT		Pk_seed, Fk_Name_seed, Ck_Size, Ck_season
+
+DROP TABLE dbo.SEEDS
+
+-- droping dbo.RESULTS table and its constraints
+
+ALTER TABLE dbo.RESULTS
+	DROP CONSTRAINT		Pk_result, Fk_product, Fk_search
+
+DROP TABLE dbo.RESULTS
+
+-- droping dbo.SEARCHES table and its constraints
+
+ALTER TABLE dbo.SEARCHES
+	DROP CONSTRAINT		Pk_search, Fk_user_srch, Ck_IP_address
+
+DROP TABLE dbo.SEARCHES
+
+-- droping dbo.USERS table and its constraints
+
+ALTER TABLE dbo.USERS
+	DROP CONSTRAINT		Pk_user, Ck_Email
+
+DROP TABLE dbo.USERS
+
+-- droping dbo.RELATIONS table and its constraints
+
+ALTER TABLE dbo.RELATIONS
+	DROP CONSTRAINT		Pk_relation, Fk_Product1, Fk_Product2
+
+DROP TABLE dbo.RELATIONS
+
+-- droping dbo.PRODUCTS table and its constraints
+
+ALTER TABLE dbo.PRODUCTS
+	DROP CONSTRAINT		Pk_product, Ck_Price, Ck_Discount
+
+DROP TABLE dbo.PRODUCTS
